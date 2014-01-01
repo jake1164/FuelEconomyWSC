e0e01426337d01b22a6674a9aca43c69a0fb7e3a
Jason Jackson <jake1164@hotmail.com>
2014-01-01 10:06:17 -0500

    Added Comments

--- FuelEconomyWSC/App_Start/WebApiConfig.cs
+++ FuelEconomyWSC/App_Start/WebApiConfig.cs
@@ -37,38 +37,11 @@ namespace NotJustMaple.WebService.FuelEconomy.API
     {
         public static void Register(HttpConfiguration config)
         {
-
+            // This combination of routes allows the api /api/Vehicle/id   /api/Vehicle/GetYears /api/Vehicle/GetModels etc...
             config.Routes.MapHttpRoute("DefaultApiWithId", "Api/{controller}/{id}", new { id = RouteParameter.Optional }, new { id = @"\d+" });
             config.Routes.MapHttpRoute("DefaultApiWithAction", "Api/{controller}/{action}");
             config.Routes.MapHttpRoute("DefaultApiGet", "Api/{controller}", new { action = "Get" }, new { httpMethod = new HttpMethodConstraint(HttpMethod.Get) });
 
-            /*
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-
-
-            config.Routes.MapHttpRoute(
-                name: "ActionApi",
-                routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new { action = "get", id = RouteParameter.Optional }
-            );
-
-            */
-
-
-
-
-
-
-            /*
-            foreach (var encoding in config.Formatters.JsonFormatter.SupportedEncodings)
-            {
-                System.Diagnostics.Trace.WriteLine(encoding.WebName);
-            }
-            */
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
             // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
--- FuelEconomyWSC/Controllers/VehicleController.cs
+++ FuelEconomyWSC/Controllers/VehicleController.cs
@@ -38,35 +38,76 @@ using NotJustMaple.WebService.FuelEconomy.API.Models;
 
 namespace NotJustMaple.WebService.FuelEconomy.API.Controllers
 {
+    /// <summary>
+    /// Class that will provide the Vehicle api that interfaces with the 
+    /// FuelEconomy Web Services. More information:
+    /// http://www.fueleconomy.gov/feg/ws/index.shtml
+    /// </summary>
     public class VehicleController : ApiController
     {
+        /// <summary>
+        /// Base webservice prefix.
+        /// </summary>
         private const String RootURI = "http://www.fueleconomy.gov/ws/rest/";
 
+        /// <summary>
+        ///  Returns a list of model years
+        /// </summary>
+        /// <returns>Json or xml String</returns>
         public MenuItemModel GetYears()
         {
             return Request<MenuItemModel>("vehicle/menu/year");
         }
 
+        /// <summary>
+        /// Return a list of makes for a particular year
+        /// </summary>
+        /// <param name="year">Model Year</param>
+        /// <returns>Json or xml String</returns>
         public MenuItemModel GetMakes(Int32 year)
         {
             return Request<MenuItemModel>(String.Format("vehicle/menu/make?year={0}", year));
         }
 
+        /// <summary>
+        /// Return a list of models for a particular year and make
+        /// </summary>
+        /// <param name="year">Model Year</param>
+        /// <param name="make">Manufacturer</param>
+        /// <returns>Json or xml String</returns>
         public MenuItemModel GetModels(Int32 year, String make)
         {
             return Request<MenuItemModel>(String.Format("vehicle/menu/model?year={0}&make={1}", year, make));
         }
 
+        /// <summary>
+        ///  Return a list of model options and the associated vehicle ID for a particular year, make and model
+        /// </summary>
+        /// <param name="year">Model Year</param>
+        /// <param name="make">Manufacturer</param>
+        /// <param name="model">Automotive Model</param>
+        /// <returns>Json or xml String</returns>
         public MenuItemModel GetOptions(Int32 year, String make, String model)
         {
             return Request<MenuItemModel>(String.Format("vehicle/menu/options?year={0}&make={1}&model={2}", year, make, model));
         }
         
+        /// <summary>
+        ///  Return a specific vehicle record
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Json or xml String</returns>
         public VehicleModel GetVehicle(Int32 id)
         {            
             return Request<VehicleModel>(String.Format("vehicle/{0}", id));
         }
 
+        /// <summary>
+        /// Method used to create the correct URI and return the correct class object
+        /// </summary>
+        /// <typeparam name="T">Class Type</typeparam>
+        /// <param name="path">URI</param>
+        /// <returns>Class Object</returns>
         [System.Web.Http.NonAction]
         public new T Request<T>(string path)
         {
